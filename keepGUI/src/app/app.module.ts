6b1c4f1add9e8397aba733a2d80bdaf129494ef0
deep-sarkar <deepsarkar96@gmail.com>
2020-10-28 11:15:33 +0530

    [Deep]Add.added imports and components to module

--- keepGUI/src/app/app.module.ts
+++ keepGUI/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule } from '@angular/router';
+import { Material } from './app.materials';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    Material,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
