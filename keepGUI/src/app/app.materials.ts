0e98428b41545e58e26c15a8c8c2312f3f08ff1c
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 08:11:28 +0530

    [Deep]Add.added inputMaterialModule in import and export

--- keepGUI/src/app/app.materials.ts
+++ keepGUI/src/app/app.materials.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
     imports : [
         MatCardModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatButtonModule
     ],
     exports : [
         MatCardModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatButtonModule
     ]
 })
 export class Material { }
