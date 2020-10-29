0a776f1157f1791d95c03a781b73f289d75cd0ed
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 21:24:57 +0530

    [Deep]Add.added validation messages for invalid email

--- keepGUI/src/app/services/validation.service.ts
+++ keepGUI/src/app/services/validation.service.ts
@@ -9,13 +9,17 @@ export class ValidationService {
 
   usernameErrorMessage(username){
     return username.hasError('required')?"Required" : 
-      username.hasError('pattern')?"atleast 4 charecter and alphabet only.":"";
+      username.hasError('pattern') ? "atleast 4 charecter and alphabet only.":"";
   }
 
   passwordErrorMessage(password){
-    return password.hasError('required')?"Required" : 
-      password.hasError('pattern')?"atleast 8 charecter and alpha-numeric only.":"";
+    return password.hasError('required') ? "Required" : 
+      password.hasError('pattern') ? "atleast 8 charecter and alpha-numeric only.":"";
   }
 
+  emailErrorMessage(email){
+    return email.hasError('required')?"Required" :
+      email.hasError('email') ? "invalid email address" : "verify email after registeration";
+  }
 
 }
