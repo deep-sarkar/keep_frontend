5e72946c960bdfcd4a6587f64cc11665073d0cbb
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 08:12:31 +0530

    [Deep]Add.added login component

--- keepGUI/src/app/components/login/login.component.ts
+++ keepGUI/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  username = new FormControl('')
+  password = new FormControl('')
+
 
   ngOnInit() {
   }
