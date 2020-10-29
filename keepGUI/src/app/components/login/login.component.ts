a6640db337596f9bd4508f6361addf8e6b5790b7
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 09:45:39 +0530

    [Deep]Add.added logic to save login token in local storage

--- keepGUI/src/app/components/login/login.component.ts
+++ keepGUI/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/services/account.service';
+import { UtilityService } from 'src/app/services/utility.service';
+import { ValidationService } from 'src/app/services/validation.service';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +11,49 @@ import { FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _accountService:AccountService,
+    private _validationMessage:ValidationService,
+    private _utility:UtilityService
+    ) { }
 
-  username = new FormControl('')
-  password = new FormControl('')
+  username = new FormControl('',[
+    Validators.required,
+    Validators.pattern("^[A-Za-z]{4,}$")
+  ])
 
+  password = new FormControl('',[
+    Validators.required,
+    Validators.pattern("^[A-Za-z0-9]{8,}$")
+  ])
+
+  validateUsername(){
+    return this._validationMessage.usernameErrorMessage(this.username)
+  }
+  
+  validatePassword(){
+    return this._validationMessage.passwordErrorMessage(this.password)
+  }
+
+
+  login(){
+    const data = {
+      "username": this.username.value,
+      "password": this.password.value
+    }
+    this._accountService.login(data)
+    .subscribe(
+      response =>{
+       if(response.code === 200){
+         localStorage.setItem(this.username.value, response.token)
+         this._utility.matMessage("Hello, " + this.username.value )
+       }
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
 
   ngOnInit() {
   }
