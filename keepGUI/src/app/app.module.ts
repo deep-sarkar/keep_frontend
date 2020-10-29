f63511183360f0f225c6cd6079d68808b9e7fb5c
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 08:12:07 +0530

    [Deep]Add.added ne logo component

--- keepGUI/src/app/app.module.ts
+++ keepGUI/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RouterModule } from '@angular/router';
 import { Material } from './app.materials';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogoComponent } from './components/logo/logo.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    LoginComponent
+    LoginComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
